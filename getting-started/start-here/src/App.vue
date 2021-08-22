056bcce2e239e2956ece7d3fef97cca1aa78fbe5
tonypang <tony.wmpang@gmail.com>
2021-08-22 22:06:06 +0800

    ch.2

--- getting-started/start-here/src/App.vue
+++ getting-started/start-here/src/App.vue
@@ -1,44 +1,18 @@
 <template>
   <div class="app">
-    <h1>This is your first component in Vue</h1>
-    <h3>{{ message }}</h3>
-    <ul>
-      <li>{{ number + 1 }}</li>
-      <li>Value of ok var is: + {{ ok ? 'true' : 'false' }}</li>
-      <li>{{ message.split('').reverse().join('') }}</li>
-    </ul>
-    <span v-if="visible">Now you see me</span>
-    <!-- <ol>
-      <li v-for="todo in todos">{{ todo.text }}</li>
-    </ol>
-    -->
-    <li>{{ message.split('').reverse().join('') }}</li>
-    <p>Original message: "{{ message }}"</p>
-    <p>Computed reversed message: "{{ reversedMessage }}"</p>
+    <h2>I am App (main component)</h2>
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
+import HelloWorld from "@/components/HelloWorld";
+//import HelloWorld from './components/HelloWorld.vue';
+
 export default {
   name: "App",
-  data: () => ({
-    message: "Powered by MDB!",
-    number: 5,
-    ok: true,
-    visible: true,
-    todos: [
-      { text: "Learn Javascript" },
-      { text: "Learn Vue" },
-      { text: "Build sth awesome" }
-    ]
-  }),
-  computed: {
-    reversedMessage: function() {
-      return this.message
-        .split("")
-        .reverse()
-        .join("");
-    }
+  components: {
+    HelloWorld
   }
 };
 </script>
---
+++ getting-started/start-here/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h3>I am Hello World Component</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<style scoped>
+h3 {
+  font-weight: normal;
+  padding-top: 20px;
+  padding-bottom: 30px;
+}
+</style>
